88463b8a3500f74180835099c76fedfae1a0dfb3
Max <maximilien01993@gmail.com>
2025-11-14 11:29:33 +0100

    Add basic user type Add basic user_store with basic getter and actions

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,16 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import type { user } from '../types/user'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const use_users_store = defineStore('user_store', {
+  state: () => ({
+    current_user: null as user | null,
+  }),
+  getters: {
+    get_current_name: (state) => state.current_user?.name,
+  },
+  actions: {
+    set_current_user(newUser: user) {
+      if (this.current_user != newUser) this.current_user = newUser
+    },
+  },
 })
