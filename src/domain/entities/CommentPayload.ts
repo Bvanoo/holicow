d396da2caf570cb286e56b7e7a52fc73f8387a9b
aleccifloriantechnobel <florian.alecci@edu.technobel.be>
2025-12-09 12:45:28 +0100

    correction CommentPayload

--- src/domain/entities/CommentPayload.ts
+++ src/domain/entities/CommentPayload.ts
@@ -2,7 +2,7 @@ import type Comment from "./Comment"
 import type Alert from "@/domain/entities/Alert.ts";
 
 export interface CommentPayload {
-  data: Alert[]
+  data: Comment[]
   total: number
   page: number
   limit: number
